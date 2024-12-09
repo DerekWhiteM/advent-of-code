9e2a6f563208492276cb6a54b038d262924b581f
dwhite2 <derekwhite.m@gmail.com>
2024-12-09 18:51:44 -0500

    day 3

--- 2024/3/2.py
+++ 2024/3/2.py
@@ -1,18 +1,24 @@
 import re
 
+pattern = r'(?:mul\(\d+,\d+\))|(?:do\(\))|(?:don\'t\(\))'
 contents = None
+
 with open('input.txt', 'r') as file:
     contents = file.read()
 
-pattern = r'mul\(\d+,\d+\)'
-matches = re.findall(pattern, contents)
 
+matches = re.findall(pattern, contents)
 result = 0
+do = True
 
 for match in matches:
-    sub_pattern = r'\d+,\d+'
-    sub_matches = re.findall(sub_pattern, match)
-    values = sub_matches[0].split(',')
-    result += (int(values[0]) * int(values[1]))
+    if match == 'do()' or match == "don't()":
+        do = match == 'do()'
+        continue
+    if do:
+        sub_pattern = r'\d+,\d+'
+        sub_matches = re.findall(sub_pattern, match)
+        values = sub_matches[0].split(',')
+        result += (int(values[0]) * int(values[1]))
 
 print(result)
