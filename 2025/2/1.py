051fdd0155eec9fa14ca29635236d6c5dd59aab0
Derek White <derekwhite.m@gmail.com>
2025-12-11 12:34:30 -0500

    Completed part 1

--- 2025/2/1.py
+++ 2025/2/1.py
@@ -1,8 +1,24 @@
+def isInvalid(num):
+    num = str(num)
+    strLen = len(num)
+    if (strLen % 2 != 0):
+        return False
+    midpoint = strLen // 2
+    firstHalf = num[:midpoint]
+    secondHalf = num[midpoint:]
+    if (firstHalf == secondHalf):
+        return True
+    return False
+
 with open('input.txt', 'r') as file:
     data = file.read()
     dataSplit = data.split(",")
-    for range in dataSplit:
-        rangeSplit = range.split("-")
-        low = rangeSplit[0]
-        high = rangeSplit[1]
-        print(low, high)
+    total = 0
+    for item in dataSplit:
+        rangeSplit = item.split("-")
+        low = int(rangeSplit[0])
+        high = int(rangeSplit[1])
+        for num in range(low, high + 1):
+            if (isInvalid(num)):
+                total += num
+    print(total)
