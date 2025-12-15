3a7b8881407127cd3b0dfbad24c42d5e38a68bd0
Derek White <derekwhite.m@gmail.com>
2025-12-14 19:17:40 -0500

    getMaxValue

--- 2025/3/1.py
+++ 2025/3/1.py
@@ -1,5 +1,17 @@
+def getMaxValue(stringArray):
+    max = stringArray[0]
+    for string in stringArray:
+        value = int(string)
+        if value > max:
+            max = value
+
+
+
+'''
 with open('input.txt', 'r') as file:
     for line in file:
         line = line.strip()
         bank = list(line)
         print(bank)
+'''
+
