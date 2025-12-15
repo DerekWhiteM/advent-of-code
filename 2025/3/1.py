2b325457f93699cfbe4b23e459f3944c13601ef4
Derek White <derekwhite.m@gmail.com>
2025-12-15 14:45:56 -0500

    Part 1 complete

--- 2025/3/1.py
+++ 2025/3/1.py
@@ -1,17 +1,26 @@
-def getMaxValue(stringArray):
-    max = stringArray[0]
-    for string in stringArray:
-        value = int(string)
+def getIndexOfMaxValue(stringArray):
+    max = int(stringArray[0])
+    maxIndex = 0
+    for i in range(len(stringArray)):
+        value = int(stringArray[i])
         if value > max:
             max = value
+            maxIndex = i
+    return maxIndex
 
+def getMaxJoltage(bank):
+    index1 = getIndexOfMaxValue(bank[:len(bank)-1])
+    value1 = bank[index1]
+    index2 = getIndexOfMaxValue(bank[index1+1:])
+    value2 = bank[index1+index2+1]
+    return int(f"{value1}{value2}") 
 
+sum = 0
 
-'''
 with open('input.txt', 'r') as file:
     for line in file:
         line = line.strip()
         bank = list(line)
-        print(bank)
-'''
+        sum += getMaxJoltage(bank)
 
+print(sum)
