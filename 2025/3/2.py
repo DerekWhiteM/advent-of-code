ee8907d2f3e719d7a2758ed910846955478627f4
Derek White <derekwhite.m@gmail.com>
2025-12-16 12:25:25 -0500

    Part 2 complete

---
+++ 2025/3/2.py
@@ -0,0 +1,24 @@
+def getMaxJoltage(bank):
+    joltageArray = bank[:12]
+    for i in range(12, len(bank)):
+        for j in range(12):
+            if (j == 12-1):
+                if (bank[i] > joltageArray[j]):
+                    joltageArray.pop(j)
+                    joltageArray.append(bank[i])
+            elif (joltageArray[j+1] > joltageArray[j]):
+                joltageArray.pop(j)
+                joltageArray.append(bank[i])
+                break
+    return int("".join(joltageArray))
+
+sum = 0
+
+with open('input.txt', 'r') as file:
+    for line in file:
+        line = line.strip()
+        bank = list(line)
+        joltage = getMaxJoltage(bank)
+        sum += joltage 
+
+print(sum)
