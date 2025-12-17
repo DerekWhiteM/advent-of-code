cd0d263cd6ae63f99cc49615e4139971b8f022c5
Derek White <derekwhite.m@gmail.com>
2025-12-17 10:17:49 -0500

    Part 1 complete

--- 2025/4/1.py
+++ 2025/4/1.py
@@ -1,9 +1,6 @@
-matrix = []
-
 def getNumAdjacent(row, col):
     maxRowIdx = len(matrix) - 1
     maxColIdx = len(matrix[0]) - 1
-
     aboveLeft = matrix[row-1][col-1] if row > 0 and col > 0 else None
     above = matrix[row-1][col] if row > 0 else None
     aboveRight = matrix[row-1][col+1] if row > 0 and col < maxColIdx else None 
@@ -12,18 +9,26 @@ def getNumAdjacent(row, col):
     below = matrix[row+1][col] if row < maxRowIdx else None
     belowLeft = matrix[row+1][col-1] if row < maxRowIdx and col > 0 else None
     left = matrix[row][col-1] if col > 0 else None
-
     adjacentTiles = [aboveLeft, above, aboveRight, right, belowRight, below, belowLeft, left]
-
     numAdjacentTiles = 0
-
     for tile in adjacentTiles:
         if (tile == '@'):
             numAdjacentTiles += 1
-
     return numAdjacentTiles
 
 with open('input.txt', 'r') as file:
+    matrix = []
     for line in file:
         matrix.append(list(line.strip()))
-    print(getNumAdjacent(0, 3))
+    total = 0
+    limit = 4
+    numRows = len(matrix)
+    numCols = len(matrix[0])
+    for i in range(numRows):
+        for j in range(numCols):
+            if matrix[i][j] != '@':
+                continue
+            numAdjacent = getNumAdjacent(i, j)
+            if numAdjacent < limit:
+                total += 1
+    print(total)
